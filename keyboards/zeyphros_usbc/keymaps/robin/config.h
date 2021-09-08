7ca9a8b57001d26666787d6169395f563af6e968
Zeyphros <robin@decker.cx>
2021-09-08 14:22:07 +0200

    Minor cleanups

--- keyboards/zeyphros_usbc/keymaps/robin/config.h
+++ keyboards/zeyphros_usbc/keymaps/robin/config.h
@@ -39,5 +39,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define PERMISSIVE_HOLD
 #define TAPPING_TERM 100
+//#define FORCE_NKRO
 
 #endif
--- keyboards/zeyphros_usbc/rev1/config.h
+++ keyboards/zeyphros_usbc/rev1/config.h
@@ -36,12 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define DIODE_DIRECTION COL2ROW
 
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* number of backlight levels */
-// #define BACKLIGHT_LEVELS 3
-
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
@@ -55,11 +49,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
-
-/* ws2812 RGB LED */
-#define RGB_DI_PIN D3
-
-#define RGBLED_NUM 16    // Number of LEDs
 
 /*
  * Feature disable options
